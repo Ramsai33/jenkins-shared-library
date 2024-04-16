7cfd8398be1edd8fe247501b9cee21b39d59a936
mudur <mudurukollaramsai335@gmail.com>
2024-04-16 07:44:16 +0530

    Sonar

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -20,10 +20,12 @@ def call() {
             stage('quality check') {
                 environment {
                     SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-                    SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                    //SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
                 }
                 steps {
                     script {
+                        SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                        wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
                         sh "sonar-scanner -Dsonar.host.url=http://172.31.18.167:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component}"
                     }
                 }
