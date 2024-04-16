52cdb099199a3194842d8d08f7af0c5a737ab60f
mudur <mudurukollaramsai335@gmail.com>
2024-04-16 06:31:18 +0530

    Sonar

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -24,7 +24,7 @@ def call() {
 
                         SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
 
-                       sh "sonar-scanner -Dsonar.host.url=http://172.31.18.167:9000 -Dsonar.login=${sonarqube.user} -Dsonar.password=${sonarqube.pass} -Dsonar.projectKey=${component}"
+                       sh "sonar-scanner -Dsonar.host.url=http://172.31.18.167:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component}"
                     }
                 }
             }
